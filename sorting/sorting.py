74db8b625f3653aff19f7c9943da715dd3587205
daniloxvalle <daniloxvalle@gmail.com>
2024-02-20 10:37:23 -0300

    feat: heap sort

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -10,6 +10,42 @@ def random_unordered_array(length=20, max_value=50) -> list:
     return unordered_array
 
 
+# Heap Sort
+# Complexity: O(n log n) - best, worst, average
+# In-place: Yes
+# Stable: No
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1  # left = 2*i + 1
+    r = 2 * i + 2  # right = 2*i + 2
+    # See if left child of root exists and is
+    # greater than root
+    if l < n and arr[i] < arr[l]:
+        largest = l
+    # See if right child of root exists and is
+    # greater than root
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+    # Change root, if needed
+    if largest != i:
+        (arr[i], arr[largest]) = (arr[largest], arr[i])  # swap
+        # Heapify the root.
+        heapify(arr, n, largest)
+
+
+def heap_sort(arr):
+    n = len(arr)
+    # Build a maxheap.
+    # Since last parent will be at (n//2) we can start at that location.
+    for i in range(n // 2, -1, -1):
+        heapify(arr, n, i)
+    # One by one extract elements
+    for i in range(n - 1, 0, -1):
+        (arr[i], arr[0]) = (arr[0], arr[i])  # swap
+        heapify(arr, i, 0)
+        print(f"{n-i}.", arr)
+
+
 # Quick Sort
 # Complexity: O(n log n) - best, average
 # Complexity: O(n^2) - worst
@@ -149,4 +185,5 @@ def selection_sort(array: list) -> list:
 # bubble_sort(random_unordered_array())
 # insertion_sort(random_unordered_array())
 # merge_sort(random_unordered_array(10, 30))
-quick_sort(random_unordered_array(10, 30))
+# quick_sort(random_unordered_array(10, 30))
+heap_sort(random_unordered_array(10, 30))
